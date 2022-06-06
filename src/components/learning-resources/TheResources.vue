e6e77f581d45efe27a270c27309b1313d9b03d87
Alex Mwangi <alex@stl-horizon.com>
2022-06-06 16:11:45 +0300

    Added :mode to each buttons ; Check which tab is active and apply null else flat ; Done using computed property methods

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,11 +1,16 @@
 <template>
     <base-card>
+        <base-button @click="setSelectedTab('stored-resources')" 
+        :mode="storedResButtonMode">
+         Stored Resources
+         </base-button>
 
-        <base-button @click="setSelectedTab('stored-resources')"> Stored Resources</base-button>
 
-
-        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
-
+        <base-button
+         @click="setSelectedTab('add-resource')"
+          :mode="addResButtonMode">
+         Add Resource
+         </base-button>
     </base-card>
 
     <component :is="selectedTab"></component>
@@ -29,7 +34,36 @@ export default {
 
         return {
             selectedTab : 'stored-resources',
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org/'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google..',
+                    link: 'https://google.com'
+                },
+            ],
         }
+    },
+
+    provide(){
+        return {
+            resources : this.storedResources
+        }
+    },
+
+    computed : {
+        storedResButtonMode(){
+            return this.selectedTab === 'stored-resources' ? null : 'flat';
+        } ,
+        addResButtonMode(){
+            return this.selectedTab === 'add-resource' ? null : 'flat';
+        } ,
     },
 
     methods : {
